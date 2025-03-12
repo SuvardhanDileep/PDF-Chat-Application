25fdc6280e2e6b517ee7c2c4c06513881ca167a7
Suvardhan Dileep <110462168+SuvardhanDileep@users.noreply.github.com>
2025-03-12 18:23:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def main():
             docs = VectorStore.similarity_search(query=query, k=3)
             #context
             # st.write(docs)
-            llm = OpenAI(model_name="gpt-3.5-turbo")
+            llm = OpenAI(model_name="gpt-4.0")
             chain = load_qa_chain(llm=llm,chain_type="stuff")
             response=chain.run(input_documents=docs,question=query)
             st.write(response)
